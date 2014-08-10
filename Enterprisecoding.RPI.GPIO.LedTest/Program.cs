3b69758f108e8b8b26b7c19d7e377708310400de
Fatih Boy <fatih@enterprisecoding.com>
2014-08-10 16:04:27 +0300

    LED sample for the pin 17 added

---
+++ Enterprisecoding.RPI.GPIO.LedTest/Program.cs
@@ -0,0 +1,63 @@
+﻿/*************************************************************************************************
+ *                   Enterprisecoding Raspberry Pi GPIO Library                                  *
+ *                        (http://enterprisecoding.com)                                          *
+ *                                                                                               *
+ * This class library is written by Fatih Boy to act as a wrapper for Gordon Hendersons WiringPi *
+ * I take no responsibility for this wrapper class providing proper functionality and give no    *
+ * warranty of any kind, nor it's use or fitness for any purpose. You use this wrapper at your   *
+ * own risk.                                                                                     *
+ *                                                                                               *
+ * This code is released as Open Source under GNU LGPL license, please ensure that you have a    *
+ * copy of the license and understand the usage terms and conditions.                            *
+ *                                                                                               *
+ * I take no credit for the underlying functionality that this wrapper provides.                 *
+ *                                                                                               *
+ * Latest version can be found at https://github.com/fatihboy/RPI.GPIO                           *
+ *************************************************************************************************/
+
+using Com.Enterprisecoding.RPI.GPIO;
+using Com.Enterprisecoding.RPI.GPIO.Enums;
+using System;
+using System.Reflection;
+
+namespace Enterprisecoding.RPI.GPIO.LedTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enterprisecoding Raspberry Pi GPIO Library v{0}", Assembly.GetEntryAssembly().GetName().Version);
+            Console.WriteLine("Copyright (c) 2014-{0} Fatih Boy", DateTime.Now.Year);
+            Console.WriteLine("This is free software with ABSOLUTELY NO WARRANTY.");
+            Console.WriteLine();
+
+            var boardInfo = WiringPi.OnBoardHardware.PiBoardInfo();
+
+            if (boardInfo.Model == PiModel.Unknown) {
+                Console.WriteLine("Your Raspberry Pi has an unknown model type. Please report this to");
+                Console.WriteLine("    fatih@enterprisecoding.com");
+                Console.WriteLine("with a copy of your /proc/cpuinfo if possible");
+            }
+            else {
+                Console.WriteLine("Raspberry Pi Details:");
+                Console.WriteLine("  Type: {0}, Revision: {1}, Memory: {2}MB, Maker: {3} {4}",
+                    boardInfo.ModelName, boardInfo.RevisionName, boardInfo.Memory, boardInfo.Maker, boardInfo.OverVolted ? "[OV]" : "");
+            }
+
+            int result = WiringPi.Core.Setup();
+
+            if (result == -1) {
+                Console.WriteLine("WiringPi init failed!");
+                return;
+            }
+
+            WiringPi.Core.PinMode(0, PinMode.Output);
+
+            Console.WriteLine("Looping Pin 0...");
+            for (; ; ) {
+                WiringPi.Core.DigitalWrite(0, DigitalValue.High);
+                System.Threading.Thread.Sleep(1000);
+
+                WiringPi.Core.DigitalWrite(0, DigitalValue.Low);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Enterprisecoding.RPI.GPIO.LedTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+﻿/*************************************************************************************************
+ *                   Enterprisecoding Raspberry Pi GPIO Library                                  *
+ *                        (http://enterprisecoding.com)                                          *
+ *                                                                                               *
+ * This class library is written by Fatih Boy to act as a wrapper for Gordon Hendersons WiringPi *
+ * I take no responsibility for this wrapper class providing proper functionality and give no    *
+ * warranty of any kind, nor it's use or fitness for any purpose. You use this wrapper at your   *
+ * own risk.                                                                                     *
+ *                                                                                               *
+ * This code is released as Open Source under GNU LGPL license, please ensure that you have a    *
+ * copy of the license and understand the usage terms and conditions.                            *
+ *                                                                                               *
+ * I take no credit for the underlying functionality that this wrapper provides.                 *
+ *                                                                                               *
+ * Latest version can be found at https://github.com/fatihboy/RPI.GPIO                           *
+ *************************************************************************************************/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Enterprisecoding Raspberry Pi GPIO Library Test")]
+[assembly: AssemblyDescription("Enterprisecoding raspberry pi GPIO library led 17 test application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Enterprisecoding.com")]
+[assembly: AssemblyProduct("RPI.GPIO")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Fatih Boy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3db80357-c781-490b-af98-7bbc09b2d481")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
